c8f76753954d0430968c3b4fe53f3d24b361945a
JohanValbuena <johan.valbuena99@gmail.com>
2024-05-24 10:11:34 -0500

    delay adjust

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-export async function fetchDataModuleOne(){
+
     try {
         const data = await import('@/app/utils/mocks/page-one.json');
         return data;
@@ -11,6 +11,9 @@ export async function fetchDataModuleOne(){
 
 export async function fetchDataModuleTwo(){
     try {
+        // artificial delay
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
         const data = await import('@/app/utils/mocks/page-two.json');
         return data;
 
